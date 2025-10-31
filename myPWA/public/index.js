a95b7865b225d1514169599b440c58c6343623aa
veronika <wawonika@gmail.com>
2025-10-31 00:49:58 +0000

    rahhhh

--- myPWA/public/index.js
+++ myPWA/public/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, "public/index.html")));
+app.use(express.static(path.join(__dirname, "index.html")));
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "public"));
