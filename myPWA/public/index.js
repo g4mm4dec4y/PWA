3211fce5475c7a3b1a2ef90ca8f28b3b223f0aca
veronika <wawonika@gmail.com>
2025-10-31 01:03:00 +0000

    testing port debugging

--- myPWA/public/index.js
+++ myPWA/public/index.js
@@ -15,7 +15,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send("Hello, World");
+    res.sendFile(path.join(__dirname, "public"));
 });
 
 app.listen(3000, () => {
