01294d11f19e1d17e194483beb459330019a9298
veronika <wawonika@gmail.com>
2025-10-31 04:52:39 +0000

    mmn

--- myPWA/public/index.js
+++ myPWA/public/index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 
 /* 
 trying to find where the double public is coming from 
