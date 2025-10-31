678b91f562d63eeba7cf06c3b95b25547d851b1f
veronika <wawonika@gmail.com>
2025-10-31 01:08:10 +0000

    b

--- myPWA/public/index.js
+++ myPWA/public/index.js
@@ -1,23 +1,9 @@
-/* const express = require("express");
+const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, "public/index.html")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
-
-app.listen(8000, () => console.log("Server is running on Port 8000, visit http://localhost:8000/ or http://127.0.0.1:8000 to access your website"));
-
-*/
-
-const express = require('express');
-const app = express();
-
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, "public"));
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+app.listen(8000, () => console.log("Server is running on Port 8000, visit http://localhost:8000/ or http://127.0.0.1:8000 to access your website") );
